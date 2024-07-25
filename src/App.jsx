a306ca783e5cf9a2974d7dd2943f9dd6264e4f66
Sainath0009 <sainathmedpalli@.com>
2024-07-25 12:05:18 +0530

    basic structure is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import Chat from "./Components/chat/Chat";
+import List from "./Components/list/List";
+import Detail from "./Components/detail/Detail";
+
+  
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+
+    <List/>
+     <Chat/>
+    <Detail/>
+    
+    
+    </div>
   )
 }
 
---
+++ src/Components/chat/Chat.jsx
@@ -0,0 +1,11 @@
+import "./chat.css"
+
+const Chat =() =>{
+
+return (
+
+    <div className="chat">Chat</div>
+)
+
+}
+export default Chat
