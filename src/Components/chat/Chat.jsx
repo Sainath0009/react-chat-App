fd96b6b609089db9550d6866909717d7f9bdc17c
Sainath0009 <sainathmedpalli@.com>
2024-07-27 16:29:46 +0530

    chat section top and bottom is created

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -4,7 +4,37 @@ const Chat =() =>{
 
 return (
 
-    <div className="chat">Chat</div>
+    <div className="chat">
+        <div className="top">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                <span> Baburoa</span>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+                </div>
+        
+            </div>
+            <div className="icons">
+                <img src="./phone.png" alt="" />
+                <img src="./video.png" alt="" />
+                <img src="./info.png" alt="" />
+
+            </div>
+        </div>
+        <div className="center"></div>
+        <div className="bottom">
+            <div className="icons">
+                <img src="./img.png" alt="" />
+                <img src="./camera.png" alt="" />
+                <img src="./mic.png" alt="" />
+            </div>
+            <input type="text" placeholder="Type a message..." />
+            <div className="emoji">
+                <img src="./emoji.png" alt="" />
+            </div>
+            <button className="sendButton">Send</button>
+        </div>
+    </div>
 )
 
 }
