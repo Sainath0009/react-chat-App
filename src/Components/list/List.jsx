aca0d2d1743e2057be067e5828086672cf6ae05f
Sainath0009 <sainathmedpalli@.com>
2024-10-14 11:24:27 +0530

    changes

--- src/Components/list/List.jsx
+++ src/Components/list/List.jsx
@@ -1,6 +1,6 @@
-import ChatList from "./chatList/ChatList"
-import "./list.css"
-import Userinfo from "./userInfo/Userinfo"
+import ChatList from "./chatList/ChatList";
+import "./list.css";
+import Userinfo from "./userInfo/Userinfo";
 
 const List = () => {
   return (
